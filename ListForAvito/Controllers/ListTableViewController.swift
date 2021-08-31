c85e6a40b150162e6196e08165df4416ea064abb
Halimat Magomedova <m.h.axn@mail.ru>
2021-08-31 09:21:07 +0300

    fix bug ListTableView

--- ListForAvito/Controllers/ListTableViewController.swift
+++ ListForAvito/Controllers/ListTableViewController.swift
@@ -7,31 +7,43 @@
 
 import UIKit
 
-class ListTableViewController: UITableViewController {
+class ListTableViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     let list = [
         List( name : "Dan", phone_number : 222),
         List( name : "Dan", phone_number : 222),
     ]
     
+    var tableView = UITableView()
+    
     let cellId = "cellId"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
+       createTable()
        
+    }
+    
+    func createTable(){
+        self.tableView = UITableView(frame: view.bounds, style: .plain )
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        
+        view.addSubview(tableView)
+        
     }
 
     // MARK: - Table view data source
 
   
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return list.count
     }
 
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
 
         let list = self.list[indexPath.row]
@@ -39,6 +51,11 @@ class ListTableViewController: UITableViewController {
         return cell
     }
     
+    // MARK: - Table view delegate
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100.0
+    }
 
    
 
