ba3dbed70ac307196002175fc54980131f7f9a66
Halimat Magomedova <m.h.axn@mail.ru>
2021-10-29 09:58:00 +0300

    big fix bug

--- Pods/Target Support Files/Pods-PetProject/Pods-PetProject-umbrella.h
+++ Pods/Target Support Files/Pods-PetProject/Pods-PetProject-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_ListForAvitoVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_ListForAvitoVersionString[];
+FOUNDATION_EXPORT double Pods_PetProjectVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_PetProjectVersionString[];
 
